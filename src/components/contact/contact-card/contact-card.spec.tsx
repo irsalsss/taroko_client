b3f7f707ec878c90682b2286b0fdecc7ba9ae89d
Muhamad Irsal Firansyah <irsal.firansyah@hypefast.id>
2024-02-22 21:16:44 +0700

    create contact card component

---
+++ src/components/contact/contact-card/contact-card.spec.tsx
@@ -0,0 +1,25 @@
+import { render } from '@/utils/test/test'
+import ContactCard from './contact-card';
+
+describe("ContactCard", () => {
+  it("should render contact card successfully", () => {
+    const { baseElement } = render(
+      <ContactCard
+        isFavorite={false}
+        onDeleteContact={jest.fn}
+        onEditContact={jest.fn}
+        onFavoriteContact={jest.fn}
+        contact={{
+          id: 1,
+          firstName: 'James',
+          lastName: 'Bond',
+          job: 'Software Engineer',
+          description: 'Cool guy from Land of Dawn'
+        }}
+      />
+    )
+
+    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ src/components/contact/contact-card/contact-card.tsx
@@ -0,0 +1,69 @@
+import AccountInterface from '@/interfaces/contact/account.interface'
+import style from './contact-card.module.scss';
+import { StarIcon, StarFilledIcon, Pencil2Icon, TrashIcon } from '@radix-ui/react-icons'
+import ButtonIcon from '@/components/shared/button-icon/button-icon';
+
+interface ContactCardProps {
+  contact: AccountInterface;
+  isFavorite?: boolean;
+  onEditContact: () => void;
+  onDeleteContact: () => void;
+  onFavoriteContact: () => void;
+}
+
+const ContactCard = ({
+  contact,
+  isFavorite,
+  onEditContact,
+  onDeleteContact,
+  onFavoriteContact
+}: ContactCardProps) => {
+  return (
+    <div className={style['contact-card-container']}>
+      <div className={style['contact-container']}>
+        <ButtonIcon
+          label={isFavorite ? 'star-filled-icon' : 'star-icon'}
+          onClick={onFavoriteContact}
+        >
+          {isFavorite ? (
+            <StarFilledIcon color='#e6b400' />
+          ) : (
+            <StarIcon color='black' />
+          )}
+        </ButtonIcon>
+
+      <div className={style['contact-other-information-container']}>
+        <span className='font-bold'>{contact.firstName + ' ' + contact.lastName}</span>
+
+        <div className={style['contact-other-information']}>
+          <span>Job:</span>
+          <span>{contact.job}</span>
+        </div>
+
+        <div className={style['contact-other-information']}>
+          <span>Description:</span>
+          <span>{contact.description}</span>
+        </div>
+      </div>
+      </div>
+
+      <div className={style['contact-action-container']}>
+        <ButtonIcon
+          label='edit-icon'
+          onClick={onEditContact}
+        >
+          <Pencil2Icon />
+        </ButtonIcon>
+
+        <ButtonIcon
+          label='delete-icon'
+          onClick={onDeleteContact}
+        >
+          <TrashIcon />
+        </ButtonIcon>
+      </div>
+    </div>
+  )
+}
+
+export default ContactCard
