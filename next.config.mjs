92dd11ce64ec54fb0da6e3ca2d04e88bc8f0bdd2
Muhamad Irsal Firansyah <irsal.firansyah@hypefast.id>
2024-02-22 17:21:45 +0700

    install sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
+import path from "path";
+import { URL } from "url";
+
+const __dirname = new URL(".", import.meta.url).pathname;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/contacts",
+          permanent: true,
+        },
+      ];
+    },
+  },
+};
 
 export default nextConfig;
