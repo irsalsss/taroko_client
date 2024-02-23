18b1e670ca4391789516a8d0fb98a696b3c47a10
Muhamad Irsal Firansyah <irsal.firansyah@hypefast.id>
2024-02-23 14:21:43 +0700

    add error props on input

--- src/components/shared/input/input.tsx
+++ src/components/shared/input/input.tsx
@@ -1,18 +1,29 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 import style from "./input.module.scss";
+import clsx from "clsx";
 
 interface InputProps extends ComponentPropsWithoutRef<"input"> {
   icon?: ReactNode;
+  error?: string;
 }
 
-const Input = (props: InputProps) => {
-  return (
-    <div className='relative'>
-      {props.icon ? (
-        <div className={style["input-icon"]}>{props.icon}</div>
-      ) : null}
+const Input = (inputProps: InputProps) => {
+  const { icon, error, ...props } = inputProps;
 
-      <input className={style["input"]} {...props} />
+  return (
+    <div className='relative flex gap-1 flex-col'>
+      {icon ? <div className={style["input-icon"]}>{icon}</div> : null}
+
+      <input
+        className={clsx(
+          style["input"],
+          icon ? style["input-padding-with-icon"] : style["input-padding"],
+          error && style["bg-error"]
+        )}
+        {...props}
+      />
+
+      {error ? <span className={style["error-message"]}>{error}</span> : null}
     </div>
   );
 };
